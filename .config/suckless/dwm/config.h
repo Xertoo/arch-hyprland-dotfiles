d20c28bc3d19f0ed072f724caf0605e1a7a3196f
Abhinav Prasai <abhinav.prsai@gmail.com>
2024-11-07 17:25:58 +0000

    fixed suckless

--- .config/suckless/dwm/config.h
+++ .config/suckless/dwm/config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx = 10;   /* gaps between windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[]    ={ "GohuFont:style=Medium:size=11" };
-static const char dmenufont[] =  "GohuFont:style=Medium:size=11";
+static const char *fonts[]    ={ "Terminus:size=11" };
+static const char dmenufont[] =  "Terminus:size=11";
 
 //DEFAULT COLOURSCHEME
 // Background color for unselected windows
@@ -96,7 +96,7 @@ static const char *dmenucmd[] = {
     "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
 static const char *termcmd[] = {"st", NULL};
 
-static const char *firefoxcmd[] = {"firefox", NULL};
+static const char *firefoxcmd[] = {"zen-browser", NULL};
 static const char *slock[] = {"slock", NULL};
 static const char *screenshotcmd[] = {
     "sh", "-c",
